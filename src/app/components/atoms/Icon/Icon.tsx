e98d2b9cb2364fe65dc0379a7e3a6b0f22aca006
hrrsnsu <hrrsnsu@gmail.com>
2023-12-04 10:08:29 -0800

    added logos correctl with links

--- src/app/components/atoms/Icon/Icon.tsx
+++ src/app/components/atoms/Icon/Icon.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import Image, { StaticImageData } from "next/image";
 import "./Icon.css";
+import Link from "next/link";
 
 type Props = {
   src: string | StaticImageData;
   alt: string;
-  href?: string;
+  href: string;
 };
 
 const Icon = (props: Props) => {
-  return <Image className="Icon" src={props.src} alt={props.alt} />;
+  return (<Link href={props.href}>
+    <Image
+      className="Icon"
+      src={props.src}
+      alt={props.alt}
+      width={25}
+      height={25}
+    />
+  </Link>);
 };
 
 export default Icon;
--- src/app/components/molecules/IconBar/IconBar.tsx
+++ src/app/components/molecules/IconBar/IconBar.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Icon from "../../atoms/Icon/Icon";
+import githubLogo from "../../../../../public/githubLogo.png";
+import linkedinLogo from "../../../../../public/linkedinLogo.png";
+import youtubeLogo from "../../../../../public/youtubeLogo.png"
+import "./IconBar.css";
 
 type Props = {};
 
 const IconBar = (props: Props) => {
   return (
     <div className="IconBar">
-      <Icon src={""} alt="github icon" href="" />
-      <Icon src={""} alt="linkedin icon" href="" />
+      <Icon src={githubLogo} alt="github icon" href="https://github.com/hrrsnsu/" />
+      <Icon src={linkedinLogo} alt="linkedin icon" href="https://linkedin.com/in/harrison-su/" />
+      <Icon src={youtubeLogo} alt="youtube icon" href="https://www.youtube.com/channel/UCzC8vViBCXSb0w9RdQ_qNnQ" />
     </div>
   );
 };
